72de8a53ff91b4ded8654ae9c28f988dcbb0b2a7
jadimebag <karolpiekara@gmail.com>
2018-06-27 18:51:34 +0200

    adding google authorized login/sing up

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'httparty'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'sidekiq'
+gem 'omniauth-google-oauth2'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :google_oauth2, '403821889792-v0sdap67aq3nbr34a7f0ucbba8je85ol.apps.googleusercontent.com', 'JJOUNA9s_5EWm2X10o540PzD', {}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
